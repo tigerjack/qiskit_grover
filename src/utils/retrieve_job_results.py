372de4c78188cca6e0a7fdeb7e034537bbe27ac9
Simone Perriello <simone.perriello@protonmail.com>
2018-12-03 19:52:58 +0100

    utils: added function to retrieve job results
    When a backend has a lot of pending jobs, our job can be postponed by
    the backend to a later time. We can retrieve the results of a postponed
    execution using the backend name and the job id printed by the
    grover_test file.

---
+++ src/utils/retrieve_job_results.py
@@ -0,0 +1,32 @@
+from qiskit import IBMQ
+import sys
+
+
+def get_job_status(backend, job_id):
+    backend = IBMQ.get_backend(backend)
+    print("Backend {0} is operational? {1}".format(
+        backend.name(),
+        backend.status()['operational']))
+    print("Backend was last updated in {0}".format(
+        backend.properties()['last_update_date']))
+    print("Backend has {0} pending jobs".format(
+        backend.status()['pending_jobs']))
+    ibmq_job = backend.retrieve_job(job_id)
+    status = ibmq_job.status()
+    print(status.name)
+    print(ibmq_job.creation_date())
+    if (status.name == 'DONE'):
+        print("EUREKA")
+        result = ibmq_job.result()
+        print("Result is: {0}".format(result.get_counts()))
+    else:
+        print("... Work in progress ...")
+        print("Queue position = {0}".format(ibmq_job.queue_position()))
+        print("Error message = {0}".format(ibmq_job.error_message()))
+
+
+IBMQ.load_accounts()
+print("Account(s) loaded")
+
+if (len(sys.argv) > 2):
+    get_job_status(sys.argv[1], sys.argv[2])
